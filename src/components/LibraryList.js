2a966cf3f252dbe7e4dd64e0a6ec880554b99f92
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2021-12-30 18:07:08 +0500

    Showing Data into a List

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {FlatList} from 'react-native';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  renderItem(library) {
+    return <ListItem library={library} />;
+  }
   render() {
-    console.log(this.props);
-    return;
+    console.log(this.props.libraries, 'dataaa');
+
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={library => library.id}
+      />
+    );
   }
 }
 
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,31 +1,26 @@
-import React , {Component} from 'react';
-import {Text, StyleSheet,View, TouchableWithoutFeedback} from 'react-native';
-import {CardSection} from './common'
-import { Actions } from 'react-native-router-flux';
+import React, {Component} from 'react';
+import {CardSection} from './common';
+import {Text} from 'react-native';
 
-class ListItem extends Component{
-    onRowPress(){
-        Actions.employeeEdit({employee:this.props.employee})
-    }
-    render(){
-        const {name}=this.props.employee;
-        return(
-            <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
-                <View>
-            <CardSection>
-                <Text style={styles.titleStyle}>
-                    {name}
-                </Text>
-            </CardSection>
-            </View>
-            </TouchableWithoutFeedback>
-        )
-    }
+class ListItem extends Component {
+  render() {
+    console.log(this.props.library, 'item data');
+    const {titleStyle} = styles;
+    return (
+      <CardSection>
+        <Text style={titleStyle}>
+          {this.props.library.item.title} - {this.props.library.item.id}
+        </Text>
+      </CardSection>
+    );
+  }
 }
-const styles = StyleSheet.create({
-    titleStyle:{
-        fontSize:18,
-        paddingLeft:15
-    }
-})
-export default ListItem
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15,
+  },
+};
+
+export default ListItem;
